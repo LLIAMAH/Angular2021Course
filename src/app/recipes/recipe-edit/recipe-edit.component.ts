f4c4a3bf4dc1c74cf00c96a8069df44d49655228
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-01-16 12:38:03 +0200

    Adding onDestroy unsubscribe (for test).

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,26 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
   private id: number = 0;
   edit: boolean = false;
+  private subscription?: Subscription;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(
+    this.subscription = this.route.params.subscribe(
       (params: Params) => {
         this.id = Number(params['id']);
         this.edit = params['id'] != null; // params['edit'] != undefined;
         console.log(this.edit);
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription!.unsubscribe();
   }
 
 }
