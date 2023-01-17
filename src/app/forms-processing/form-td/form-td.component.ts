00ff64c16fba0d27805fb2c1dae0d565dcb3f2f8
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-01-17 18:20:46 +0200

    Default TD form submit.

--- src/app/forms-processing/form-td/form-td.component.ts
+++ src/app/forms-processing/form-td/form-td.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+export enum Gender {
+  Unknown,
+  Male,
+  Female
+}
 
 @Component({
   selector: 'app-form-td',
@@ -6,5 +13,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-td.component.css']
 })
 export class FormTdComponent {
+  @ViewChild('f') signUpFom!: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  gender: Gender = Gender.Unknown;
+  genders = Object.values(Gender);
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    const username = form.value.username;
+    const email = form.value.email;
+    const secret = form.value.secret;
+
+    console.log(form.valid);
+    console.log(`${username}:${email}:${secret}`);
+  }
+
+  onSuggestedClick(): void {
+    this.signUpFom.form.patchValue({userData:{username: 'Superuser'}});
+    /*this.signUpFom.setValue({
+      userData: {
+        username: 'Superuser',
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      genderType: 'Male'
+    });*/
+  }
+
+  isAllowedGender(input: any) {
+    return (typeof input === 'string' || input instanceof String) && input !== 'Unknown';
+  }
 
 }
