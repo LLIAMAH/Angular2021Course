c22cb4d9951cbb3a05861fbbd3d6904799f420a9
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-01-31 13:08:55 +0200

    Recipe finished and HTTP in process.

--- src/app/services/recipes.resolver.ts
+++ src/app/services/recipes.resolver.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
 import {DataStorageService} from "./data-storage.service";
-import {IResponseRecipes} from "../experiments-http/types/IResponsePost";
+import {IResponseRecipes, ResponseRecipes} from "../experiments-http/types/IResponsePost";
+import {RecipeService} from "./recipe.service";
+import {EnumResponseStatus, ResponseStatus} from "../general-types/IResponses";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolver implements Resolve<IResponseRecipes> {
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService) {
   }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IResponseRecipes> {
-    return this.dataStorageService.fetchRecipes();
-
+    const recipes = this.recipesService.getRecipes()
+    if (recipes.length === 0)
+      return this.dataStorageService.fetchRecipes();
+    else
+      return of(new ResponseRecipes(recipes, new ResponseStatus(EnumResponseStatus.OK)));
   }
 }
